636f3cbb579e29c61032c19ccad79f31b209b235
GiaKhanh-LVC <giakhanhcafetung@gmail.com>
2024-11-10 01:03:46 +0700

    servo3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/ServoControlModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/ServoControlModule.java
@@ -3,5 +3,5 @@ package org.firstinspires.ftc.teamcode.control;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class ServoControlModule {
-    private Servo servo1, servo2;
+    private Servo servo1, servo2,servo3;
 }
