7b684cbdddae0c49ceaed5b78f55f56eeac64951
mihduc-18 <sumun1822@gmail.com>
2024-11-10 09:48:30 +0700

    code cho servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/ServoControlModule.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.control;
-
-import com.qualcomm.robotcore.hardware.Servo;
-
-public class ServoControlModule {
-    private Servo servo1, servo2,servo3, servo4;
-}
